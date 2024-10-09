a4b7abab671cb7d30138cfe7ed599483c16eae01
Amenakter <amenakter27@gmail.com>
2024-10-09 13:32:28 +0600

    this is first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">
+          Hello World! I am Amena
+        </h1>
+        <div>Fisrt div</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
